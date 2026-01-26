5cdedf120727d9061b8d95b1c19beec5006e6bf0
Ngoc Trong <trongpvn2211@gmail.com>
2026-01-26 17:51:51 +0700

    setup project

---
+++ src/main/java/vn/motoCare/MotoCareApplication.java
@@ -0,0 +1,13 @@
+package vn.motoCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/motoCare/MotoCareApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.motoCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
